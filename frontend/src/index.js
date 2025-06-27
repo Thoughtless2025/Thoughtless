2128b24660985c2d29e1b0c2a408b11c25054730
Thoughtless2025 <thoughtless2025@gmail.com>
2025-06-27 22:36:05 +0100

    Update index.js
    Add gemini code but need to sort out project id

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,2 +1,36 @@
 --placeholder
 firebase project id thoughtless-7aacc
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css'; // You'll create this or a similar CSS file
+import App from './App';
+import reportWebVitals from './reportWebVitals';
+import { BrowserRouter } from 'react-router-dom';
+import { initializeApp } from 'firebase/app'; // For Firebase initialization
+
+// Your Firebase project configuration
+const firebaseConfig = {
+  apiKey: "YOUR_FIREBASE_API_KEY", // This is usually public for client-side apps
+  authDomain: "YOUR_FIREBASE_PROJECT_ID.firebaseapp.com",
+  projectId: "YOUR_FIREBASE_PROJECT_ID",
+  storageBucket: "YOUR_FIREBASE_PROJECT_ID.appspot.com",
+  messagingSenderId: "YOUR_MESSAGING_SENDER_ID",
+  appId: "YOUR_APP_ID"
+};
+
+// Initialize Firebase
+initializeApp(firebaseConfig);
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </React.StrictMode>
+);
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();
