d5850723212135c4fb31c395a6278f82c771b75c
thoughtless2025 <thoughtless2025@gmail.com>
2025-07-19 20:10:48 +0000

    feat: Add basic index.html and script.js for web app

---
+++ script.js
@@ -0,0 +1,19 @@
+// TODO: Add Firebase configuration
+// const firebaseConfig = {
+//   apiKey: "YOUR_API_KEY",
+//   authDomain: "YOUR_AUTH_DOMAIN",
+//   projectId: "YOUR_PROJECT_ID",
+//   storageBucket: "YOUR_STORAGE_BUCKET",
+//   messagingSenderId: "YOUR_MESSAGING_SENDER_ID",
+//   appId: "YOUR_APP_ID"
+// };
+
+// TODO: Initialize Firebase
+// firebase.initializeApp(firebaseConfig);
+
+// TODO: Get Firestore instance
+// const db = firebase.firestore();
+
+// TODO: Implement function to add chat data to Firestore
+
+// TODO: Implement function to display chat data from Firestore
